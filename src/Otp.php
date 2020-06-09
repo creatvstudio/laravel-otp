b291e952e8e758e6a1a9b174c130e3c9630fdb27
maganasamuel <maganasamuel001@gmail.com>
2020-06-09 17:14:26 +0800

    moved routes static function to otp facades

--- src/Otp.php
+++ src/Otp.php
@@ -4,11 +4,6 @@ namespace CreatvStudio\Otp;
 
 class Otp
 {
-    public static function routes()
-    {
-        // Add routes like Passport::routes();
-    }
-
     public static function session()
     {
         // return the current otp session
