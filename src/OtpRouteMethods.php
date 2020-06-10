0044534cef93b0539bf0d39f5b4569c6ebdee316
maganasamuel <maganasamuel001@gmail.com>
2020-06-10 17:03:46 +0800

    Added resend option for route methods

--- src/OtpRouteMethods.php
+++ src/OtpRouteMethods.php
@@ -7,9 +7,12 @@ class OtpRouteMethods
     public function otp()
     {
         return function ($options = []) {
-            $this->get('otp', 'Otp\OtpController@index')->name('otp.index');
+            $this->get('otp/verify', 'Otp\OtpController@index')->name('otp.index');
             $this->post('otp/verify', 'Otp\OtpController@verify')->name('otp.verify');
-            $this->post('otp/resend', 'Otp\OtpController@resend')->name('otp.resend');
+
+            if ($options['resend'] ?? true) {
+                $this->post('otp/resend', 'Otp\OtpController@resend')->name('otp.resend');
+            }
         };
     }
 }
