e7ce4a0fc2e983781cba80f7be9cb221c5af86ca
maganasamuel <maganasamuel001@gmail.com>
2020-06-24 14:49:20 +0800

    Updated routes, session, and redirection for otp functionality

--- src/Otp.php
+++ src/Otp.php
@@ -2,17 +2,47 @@
 
 namespace CreatvStudio\Otp;
 
+use CreatvStudio\Otp\RedirectsUsers;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
 
 class Otp
 {
+    use RedirectsUsers;
+
     public function routes($options = [])
     {
-        Route::get('otp/verify', 'OtpController@index')->name('otp.index');
-        Route::post('otp/verify', 'OtpController@verify')->name('otp.verify');
+        Route::prefix('otp')->name('otp.')->group(function () {
+            Route::prefix('verify')->group(function () {
+                Route::get('', 'OtpController@index')->name('index');
+                Route::post('', 'OtpController@verify')->name('verify');
+            });
 
-        if ($options['resend'] ?? true) {
-            Route::post('otp/resend', 'OtpController@resend')->name('otp.resend');
-        }
+            if ($options['resend'] ?? true) {
+                Route::post('resend', 'OtpController@resend')->name('resend');
+            }
+
+            Route::prefix('once')->name('once.')->group(function () {
+                Route::get('', 'OtpOnceController@index')->name('index');
+                Route::post('', 'OtpOnceController@verify')->name('verify');
+
+                if ($options['resendOnce'] ?? true) {
+                    Route::post('resend', 'OtpOnceController@resend')->name('resend');
+                }
+            });
+        });
+    }
+
+    public function rememberOtpOnce()
+    {
+        Session::put('otp_once', Str::random(60));
+    }
+
+    public function redirectOtpOnce()
+    {
+        $this->rememberOtpOnce();
+
+        return redirect()->intended($this->redirectPath())->with('_old_input', Session::get('otp_once_input'));
     }
 }
